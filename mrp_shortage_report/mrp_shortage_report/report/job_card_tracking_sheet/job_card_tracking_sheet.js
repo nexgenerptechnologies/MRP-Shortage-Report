6d4d459d800660b5db619b84637c74f76c7bb7d2
Regis Digital <regissdigital@gmail.com>
2026-07-16 20:34:32 +0530

    Add Job Card Tracking Sheet report

---
+++ mrp_shortage_report/mrp_shortage_report/report/job_card_tracking_sheet/job_card_tracking_sheet.js
@@ -0,0 +1,42 @@
+frappe.query_reports["Job Card Tracking Sheet"] = {
+    "filters": [
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
+        },
+        {
+            "fieldname": "work_order",
+            "label": __("Work Order"),
+            "fieldtype": "Link",
+            "options": "Work Order"
+        },
+        {
+            "fieldname": "item_code",
+            "label": __("Item Code"),
+            "fieldtype": "Link",
+            "options": "Item"
+        },
+        {
+            "fieldname": "operation",
+            "label": __("Operation"),
+            "fieldtype": "Link",
+            "options": "Operation"
+        }
+    ]
+};
---
+++ mrp_shortage_report/mrp_shortage_report/report/job_card_tracking_sheet/job_card_tracking_sheet.py
@@ -0,0 +1,77 @@
+import frappe
+from frappe import _
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    return [
+        {"fieldname": "date", "label": _("Date"), "fieldtype": "Date", "width": 100},
+        {"fieldname": "work_order", "label": _("Work Order"), "fieldtype": "Link", "options": "Work Order", "width": 150},
+        {"fieldname": "status", "label": _("Status"), "fieldtype": "Data", "width": 100},
+        {"fieldname": "item_code", "label": _("Item Code"), "fieldtype": "Link", "options": "Item", "width": 120},
+        {"fieldname": "item_name", "label": _("Item Name"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "operation", "label": _("Operation"), "fieldtype": "Link", "options": "Operation", "width": 120},
+        {"fieldname": "operator_name", "label": _("Operator Name"), "fieldtype": "Data", "width": 120},
+        {"fieldname": "workstation", "label": _("Workstation"), "fieldtype": "Data", "width": 120},
+        {"fieldname": "operation_time_mints", "label": _("Operation Time (Mints)"), "fieldtype": "Float", "width": 160},
+        {"fieldname": "shift", "label": _("Shift"), "fieldtype": "Data", "width": 100},
+        {"fieldname": "production_qty_pcs", "label": _("Production Qty (Pcs)"), "fieldtype": "Float", "width": 160},
+        {"fieldname": "production_qty_kgs", "label": _("Production Qty (Kgs)"), "fieldtype": "Float", "width": 160},
+        {"fieldname": "scrap_kgs", "label": _("Scrap (Kgs)"), "fieldtype": "Float", "width": 120},
+        {"fieldname": "total_required", "label": _("Total Required"), "fieldtype": "Float", "width": 120},
+        {"fieldname": "total_received", "label": _("Total Received"), "fieldtype": "Float", "width": 120},
+        {"fieldname": "pending_to_receive", "label": _("Pending to Receive"), "fieldtype": "Float", "width": 140},
+        {"fieldname": "qty_consumed", "label": _("Qty Consumed"), "fieldtype": "Float", "width": 120},
+        {"fieldname": "available_on_floor", "label": _("Available on Floor"), "fieldtype": "Float", "width": 140},
+        {"fieldname": "jc_completed", "label": _("JC Completed"), "fieldtype": "Float", "width": 120},
+        {"fieldname": "balance_to_complete_jc", "label": _("Balance to Complete JC"), "fieldtype": "Float", "width": 160}
+    ]
+
+def get_data(filters):
+    conditions = []
+    
+    if filters.get("from_date"):
+        conditions.append(f"jc.posting_date >= '{filters.get('from_date')}'")
+    if filters.get("to_date"):
+        conditions.append(f"jc.posting_date <= '{filters.get('to_date')}'")
+    if filters.get("company"):
+        conditions.append(f"jc.company = '{filters.get('company')}'")
+    if filters.get("work_order"):
+        conditions.append(f"jc.work_order = '{filters.get('work_order')}'")
+    if filters.get("item_code"):
+        conditions.append(f"jc.production_item = '{filters.get('item_code')}'")
+    if filters.get("operation"):
+        conditions.append(f"jc.operation = '{filters.get('operation')}'")
+
+    where_clause = " AND ".join(conditions) if conditions else "1=1"
+
+    query = f"""
+        SELECT
+            jc.posting_date as date,
+            jc.work_order,
+            jc.status,
+            jc.production_item as item_code,
+            jc.item_name,
+            jc.operation,
+            wo.qty as total_required,
+            wo.material_transferred_for_manufacturing as total_received,
+            wo.qty - wo.material_transferred_for_manufacturing as pending_to_receive,
+            wo.produced_qty as qty_consumed,
+            wo.material_transferred_for_manufacturing - wo.produced_qty as available_on_floor,
+            jc.total_completed_qty as jc_completed,
+            jc.for_quantity - jc.total_completed_qty as balance_to_complete_jc
+        FROM
+            `tabJob Card` jc
+        INNER JOIN
+            `tabWork Order` wo ON jc.work_order = wo.name
+        WHERE
+            {where_clause}
+            AND jc.docstatus < 2
+        ORDER BY
+            jc.posting_date DESC, jc.work_order ASC
+    """
+
+    return frappe.db.sql(query, as_dict=1)
